561890a7dbf6af7fb216f2d41bc349f2fb0d3671
Tony <AVStarikovich@ya.ru>
2017-06-12 13:16:26 +0300

    initial server

---
+++ index.js
@@ -0,0 +1,10 @@
+let app = require('express')();
+let http = require('http').Server(app);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
